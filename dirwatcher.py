c4bcae4a41e31b7f795c52f27fd200ec08e5a5a5
Clinton Johnson <dragoon1246224@gmail.com>
2019-04-08 16:47:07 -0400

    Refactored and debugged code.

--- dirwatcher.py
+++ dirwatcher.py
@@ -6,10 +6,10 @@ Dirwatcher- Create a long running program
 that searches a directories files for text files
 containing an input string,
 if found, log the txt file and line found.
-use python dirwatcher.py {search string} {file type} {directory to search}
+use python dirwatcher.py {extension} {polling interval} {directory to search} {magic text}
 """
 
-__author__ = "Clinton Johnson"
+'''When I do a refactor try to elimate all the global variables except for the exit_flag'''
 
 import os
 import argparse
@@ -18,30 +18,42 @@ import datetime
 import logging
 import signal
 
+__author__ = "Clinton Johnson"
+
+
 """Boiler plate for logger on global/module level """
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(__name__)
+
 """Exit flag part of sys.signal """
 exit_flag = False
 files_logged = []
 found_text = {}
 
+# def scan_file(file_name, starting_line, magic_text):
+    '''open file to read
+    enumerate through the lines in a file using a for loop
+    When line number reaches starting line that is when you start searching for the magic_text
+    Every time it finds the magic text in lines and subsequent lines it will log that it found the text.
+    Return last line that was ended on.
+    '''
+
 
 def find_files(directory, extension, magictext):
-    """Find all files in given directory, if dir exists """
+    """Find all files in given directory, if dir exists"""
     global files_logged
     global found_text
-    directory_abspath = os.path.abspath(directory)
-    all_files_in_directory = os.listdir(directory_abspath)
-    for file in all_files_in_directory:
+    dir_path = os.path.abspath(directory)
+    dir_files = os.listdir(dir_path)
+    for file in dir_files:
         if file.endswith(extension) and file not in files_logged:
             logger.info('New file found: {}'.format(file))
             files_logged.append(file)
         if file.endswith(extension):
-            file_path = os.path.join(directory_abspath, file)
+            file_path = os.path.join(dir_path, file)
             if find_string_in_files(file_path, magictext):
                 break
     for file in files_logged:
-        if file not in all_files_in_directory:
+        if file not in dir_files:
             logger.info('File deleted: {}'.format(file))
             files_logged.remove(file)
             found_text[file] = 0
@@ -67,17 +79,13 @@ def find_string_in_files(file, magictext):
                     return True
 
 
-def log_config():
-    """Adjusts how info is displayed in log"""
+def logger_initiate():
+    """Adjusts how info is displayed in log and sets default log"""
+    logger.setLevel(logging.DEBUG)
     return logging.basicConfig(
         format=(
             '%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s'),
         datefmt='%Y-%m-%d %H:%M:%S')
-
-
-def log_set_level():
-    """Sets defaulf log level"""
-    logger.setLevel(logging.DEBUG)
 
 
 def logger_banner(startorend, time, start=True):
@@ -96,12 +104,6 @@ def logger_banner(startorend, time, start=True):
     )
 
 
-def logger_initiate():
-    """Iniates the logger boiler plate"""
-    log_config()
-    log_set_level()
-
-
 def signal_handler(sig_num, frame):
     """Smooth exit from system"""
     global exit_flag
@@ -111,50 +113,43 @@ def signal_handler(sig_num, frame):
 
 
 def create_parser():
-    """Create Parser that accepts"""
-    parser = argparse.ArgumentParser(description='Taking names')
-    parser.add_argument('magictext', help='Text to search for')
-    parser.add_argument(
-        'extension', help='Type of tile extension to search')
-    parser.add_argument('directory', help='Directory to watch')
-    parser.add_argument(
-        'poll', help="polling interval required: number of seconds",
-        nargs='?', default=1)
+    """Create Parser that accepts {optional extension} {optional polling interval} {directory to search} {magic text}"""
+    parser = argparse.ArgumentParser(description='Watching for files containing magictext')
+    parser.add_argument('--ext', help='File extensions to filter on, default=.txt', default='.txt')
+    parser.add_argument('--poll', help="Polling interval in seconds, default=1.0", type=float, default=1.0)
+    parser.add_argument('directory', help='Directory to watch.')
+    parser.add_argument('magictext', help='Text to search for within matching files.')
     return parser
 
 
-def main(input_args):
+def main():
     """Currently only prints the files in a directory with extention"""
+    parser = create_parser()
+    input_args = parser.parse_args()
+    if not input_args:
+        parser.print_usage()
+        sys.exit(1)
+    logger_initiate()
+    start_time = datetime.datetime.now()
+    logger_banner('Started', start_time)
+
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    count = 0
+    logger.info('Searching directory="{0}" ext="{1}" interval="{2}" text="{3}" '.format(input_args.directory, input_args.ext, input_args.poll, input_args.magictext))
     while not exit_flag:
-        logger.debug('Searching file="{0}" ext="{1}" text="{2}" seconds={3}'
-                     .format(input_args.directory, input_args.extension,
-                             input_args.magictext, input_args.poll))
         try:
-            # if os.path.isdir(input_args.directory):
             find_files(input_args.directory,
-                       input_args.extension, input_args.magictext)
-            count = 0
+                       input_args.ext, input_args.magictext)
         except OSError as e:
-            if not count:
-                logger.error('Directory does not exist: {}'.format(e))
-                count += 1
+            logger.error('Directory does not exist: {}'.format(e))
         except Exception as e:
-            if not count:
-                logger.error('Unknown/unhandled error: {}'.format(e))
-        time.sleep(int(input_args.poll))
+            logger.error('Unknown/unhandled error: {}'.format(e))
+        time.sleep(input_args.poll)
+
+    total_time = datetime.datetime.now() - start_time
+    logger_banner('Ended', total_time, start=False)
 
 
 if __name__ == "__main__":
     """Accepts parser, initiates logger, runs banners"""
-    # python dirwatcher.py 3 string '.txt' searchhere
-    parser = create_parser()
-    input_args = parser.parse_args()
-    logger_initiate()
-    start_time = datetime.datetime.now()
-    logger_banner('Started', start_time)
-    main(input_args)
-    total_time = datetime.datetime.now() - start_time
-    logger_banner('Ended', total_time, start=False)
+    main()
